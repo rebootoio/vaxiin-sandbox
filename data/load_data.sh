e4292c513f1c34866c736feca4469d8558548d1c
Alex Balk <alex@rebooto.io>
2022-04-07 16:56:44 +0300

    make load data locate vaxctl

--- data/load_data.sh
+++ data/load_data.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
+
+error_exit() {
+  echo "ERROR: $1"
+  exit 1
+}
+
+
+if $(which vaxctl > /dev/null 2>&1); then
+  VAXCTL="vaxctl"
+else
+  if [ -f "./vaxctl" ]; then
+    VAXCTL="./vaxctl"
+    if [ ! -x $VAXCTL ]; then
+      error_exit "$VAXCTL not set as executable"
+    fi
+  else
+    error_exit "vaxctl not found in PATH or in local directory"
+  fi
+fi
+
 for category in cred device action state rule; do
-  cd $category
-  for resource in *; do
-    if [ -f $resource ]; then
-      echo $category/$resource
-      vaxctl apply $category -f $resource
+  for resource in $(dirname $0)/${category}/*; do
+    if [ -f ${resource} ]; then
+      echo $resource
+      $VAXCTL apply $category -f ${resource}
     fi
   done
-  cd ..
 done
